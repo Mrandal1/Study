4b25fb97acd61fbab0eb29f17e00341710d40705
Mrandal1 <1160707106@qq.com>
2020-04-25 19:30:38 +0800

    4.25

---
+++ src/study/chapter05/Test05.java
@@ -0,0 +1,13 @@
+package study.chapter05;
+
+/**
+ * @author Randal1
+ * @version V1.0
+ * @Package study.chapter05
+ * @data 2020/4/25 17:51
+ */
+public class Test05 {
+    public static void main(String[] args) {
+
+    }
+}
